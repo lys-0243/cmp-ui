641d1a4f46bc6547ad9ce8db6c93e6d8a39914c4
Sylvain Tshiasuma <sylvain.tshiasuma2017@gmail.com>
2023-08-03 19:46:33 +0100

    custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'black': ['MBlack', 'sans-serif'],
+        'bold': ['MBold', 'sans-serif'],
+        'medium': ['MMedium', 'sans-serif'],
+        'regular': ['MRegular', 'sans-serif'],
+        'light': ['MLight', 'sans-serif'],
+      },
     },
   },
   plugins: [],
